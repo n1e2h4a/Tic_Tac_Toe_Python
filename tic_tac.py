a6372fc8cd676379a9d689e3572f41c0b9132997
niharika <guptaniharika851@gmail.com>
2020-09-04 00:02:24 +0530

    ADDED:Determine after every move the winner or tie or change the trurn

--- tic_tac.py
+++ tic_tac.py
@@ -1,3 +1,14 @@
+
+def myBoard(tiles):
+    # "board" is a list of 10 strings representing the board (ignore index 0)
+    print(' ' + tiles[7] + ' | ' + tiles[8] + '  | ' + tiles[9])
+    print('---+----+---')
+    print(' ' + tiles[4] + ' | ' + tiles[5] + '  | ' + tiles[6])
+    print('---+----+---')
+    print(' ' + tiles[1] + ' | ' + tiles[2] + '  | ' + tiles[3])
+
+
+
 def inputPlayerSymbol():
     # Player will decide which symbol he/she prefer
 
