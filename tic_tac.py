b29c62fc7885ed107b42d39dab4e50a292b732b4
niharika <guptaniharika851@gmail.com>
2020-09-03 19:49:14 +0530

    ADDED:Determine the symbol X or O to the player

--- tic_tac.py
+++ tic_tac.py
@@ -1,2 +1,18 @@
+def inputPlayerSymbol():
+    # Player will decide which symbol he/she prefer
+
+    Symbol = ''
+
+    while not (Symbol == 'X' or Symbol == 'O'):
+        print('what are you want to take X or O?')
+        Symbol = input().upper()
+
+    # Returns a list with the player’s symbol and the computer's symbol.
+
+    if Symbol == 'X':
+        return ['X', 'O']
+    else:
+        return ['O', 'X']
+
 print("Start a fresh game Tic Tac Toe!")
 
